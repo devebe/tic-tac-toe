f764999a0e5ffff97ebe6ff95c1ab8063e43fabe
devebe <dvbirdja@gmail.com>
2023-10-23 23:25:53 +0200

    Refactor view module

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const dataModule = (() => {
 
     const board = [];
 
-    const tileFactory = (tilePosition) => {
+    const tileFactory = (position) => {
         let owner = "-";
-        let isChecked = false;
-        return {tilePosition, owner, isChecked};
+        return {position, owner};
     };
     
     const buildBoard = (tiles) => {
@@ -20,12 +19,11 @@ const dataModule = (() => {
 
     const playerFactory = (name) => {
         let ownsTiles = [];
-        const setTile = (tilePosition) => {
-            let tile = board[tilePosition - 1];
+        const setTile = (position) => {
+            let tile = board[position];
             if (tile.owner == "-") {
-                tile.isChecked = true;
                 tile.owner = name;
-                ownsTiles.push(tile.tilePosition);
+                ownsTiles.push(tile.position);
                 return true;
             };
             return false;
@@ -39,26 +37,31 @@ const dataModule = (() => {
 
 const viewModule = (() => {
 
-    const viewTile = (parent, name, text, tilePosition) => {
-        const child = document.createElement('div');
-        parent = document.querySelector(parent);
-        child.setAttribute('class', `${name}`);
-        child.setAttribute('id', `${tilePosition}`);
-        child.textContent = text;
+    function Component(name, parentId) {
+        this.name = name;
+        this.id;
+        this.tagType;
+        this.parentId = parentId;
+    };
+    
+    const buildComponent = (component) => {
+        const child = document.createElement(`${component.tagType}`);
+        child.setAttribute('id',`${component.id}`);
+        child.setAttribute('class',`${component.name}`);
+        const parent = document.getElementById(`${component.parentId}`);
         parent.appendChild(child);
     };
-
-    const buildBoard = (parent, name, text, tiles) => {
-        for (let i = 0; i < tiles; i++) {
-            viewTile(parent, name, text, (i + 1));
+    
+    const buildBoard = (numberOfTiles) => {
+        for (let i = 0; i < numberOfTiles; i++) {
+            const tile = new Component('tile','gameboard');
+            tile.tagType = 'div';
+            tile.id = i
+            buildComponent(tile);
         };
     };
 
-    const viewInfoMessage = (message) => {
-        viewTile('div.alert', 'gameresult', message, '');
-    }
-
-    return {buildBoard, viewInfoMessage};
+    return {buildBoard};
 
 })();
 
@@ -72,7 +75,7 @@ const controllerModule = (() => {
 
     const buildGame = (tiles) => {
         dataModule.buildBoard(tiles);
-        viewModule.buildBoard('div#gameboard','tile','',tiles);
+        viewModule.buildBoard(tiles);
     };
 
     buildGame(9);
@@ -80,7 +83,7 @@ const controllerModule = (() => {
     gameboard.addEventListener('click', (e) => {
         if ( dice == 'X') {
             let bool = playerX.setTile(e.target.id);
-            e.target.textContent = dataModule.board[e.target.id - 1].owner;
+            e.target.textContent = dataModule.board[e.target.id].owner;
             if(bool == true) {
                 dice = 'O';
                 turns++;
@@ -88,7 +91,7 @@ const controllerModule = (() => {
         }
         else {
             let bool = playerO.setTile(e.target.id);
-            e.target.textContent = dataModule.board[e.target.id - 1].owner;
+            e.target.textContent = dataModule.board[e.target.id].owner;
             if(bool == true) {
                 dice = 'X';
                 turns++;
@@ -98,25 +101,26 @@ const controllerModule = (() => {
     });
 
     const winStates = [
+        [1,5,9], [3,5,7],
         [1,2,3], [4,5,6], [7,8,9],
-        [1,4,7], [2,5,8], [3,6,9],
-        [1,5,9], [3,5,7]
+        [1,4,7], [2,5,8], [3,6,9] 
     ];
 
     const checkWinner = (arrX, arrO, winStates,turns) => {
         for (let i = 0; i < winStates.length; i++) {
             let winnerX = winStates[i].every(element => arrX.includes(element));
             if (winnerX == true){
-                viewModule.viewInfoMessage('Player X has won');
+                console.log('Player X has won');
             };
             let winnerO = winStates[i].every(element => arrO.includes(element));
             if (winnerO == true){
-                viewModule.viewInfoMessage('Player O has won');
+                console.log('Player O has won');
             };
-            if (turns == 9 && winnerO == false && winnerX ==false) {
-                viewModule.viewInfoMessage('It is a tie');
+            if (turns == 9 && winnerO == false && winnerX == false) {
+                console.log('It is a tie');
+                break;
             };
-        };   
+        };  
     };
 
     btn.addEventListener('click', (e) => {
